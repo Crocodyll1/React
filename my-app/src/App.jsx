c377e148545e7052ad0286d77065dec72ae3a102
Sushant <unknownsushant.0042@gmail.com>
2026-01-26 23:58:05 +0530

    resuable components

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,18 @@
 import SubsButton from "./subsbutton";
 import Hello from "./hello";
+import Random from "./random";
 
 function App(){
   return <div>
    <h1>Hey Sushant!</h1>
     <SubsButton></SubsButton>
     <Hello></Hello>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
   </div>
 }
 
---
+++ my-app/src/random.jsx
@@ -0,0 +1,7 @@
+function Random(){
+     
+    let number = Math.random() * 100;
+    
+    return <h3 style={{'background-color':'#549039'}}>random number is : {Math.round(number)}</h3>
+}
+export default Random;
